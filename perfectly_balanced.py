44d2c0068a5a25b9ff6d4865194e899e3c29ecf0
perryc85 <cjperry85@outlook.com>
2019-01-16 18:31:28 -0500

    uploading changes

--- perfectly_balanced.py
+++ perfectly_balanced.py
@@ -2,10 +2,16 @@
 user_input = input('Enter a string of only x\'s and y\'s: ')
 
 # count the number of x's and y's
-x_count = user_input.count('x')
-y_count = user_input.count('y')
+x_count = ''
+y_count = ''
 
-isBalance = x_count == y_count
+for chars in user_input:
+    if chars == 'x':
+        x_count += chars
+    else:
+        y_count += chars
+    
+isBalance = len(x_count) == len(y_count)
 
 def check_balance():
     if isBalance:
@@ -15,3 +21,8 @@ def check_balance():
 
 result = check_balance()
 print(result)
+
+
+
+# x_count = user_input.count('x')
+# y_count = user_input.count('y')
