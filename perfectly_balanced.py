29fbbd29abbd0d2db87379777c8a26ba7ff6059d
perryc85 <cjperry85@outlook.com>
2019-01-23 23:02:11 -0500

    another update

--- perfectly_balanced.py
+++ perfectly_balanced.py
@@ -1,16 +1,19 @@
+def Dict(inputString):
+    counts = dict()
+    for chars in inputString:
+        counts[chars] = counts.get(chars,0) + 1
+    return counts
+    
+def isTrueOrFalse(counts):
+    prev_val = None
+    for value in counts.values():
+        if prev_val != None and prev_val != value:
+            return False
+        prev_val = value
+    return True
+
 # Bonus 
 # get input
-user_input = input('Enter a string : ')
+user_input = input('Enter a string: ')
 
-counts = dict()
-for chars in user_input:
-    counts[chars] = counts.get(chars,0) + 1
-
-value_count = []
-for value in counts.values():
-    value_count.append(value)
-
-if value_count[1:] == value_count[:-1]:
-    print('true')
-else:
-    print('false')
+print(isTrueOrFalse(Dict(user_input)))
